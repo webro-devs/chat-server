b7f99c6ac250fa298ebb6ad8e7f91bfb1eee8914
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-11-02 00:16:54 +0500

    feat: starter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { MessageModule } from './modules/message/message.module';
+import { FolderModule } from './modules/folder/folder.module';
+import { ChatModule } from './modules/chat/chat.module';
+import { GroupModule } from './modules/group/group.module';
+import { ChannelModule } from './modules/channel/channel.module';
+import { ContactModule } from './modules/contact/contact.module';
+import { WebsocketModule } from './modules/websocket/websocket.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    MessageModule,
+    FolderModule,
+    ChatModule,
+    GroupModule,
+    ChannelModule,
+    ContactModule,
+    WebsocketModule,
+    PrismaModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
