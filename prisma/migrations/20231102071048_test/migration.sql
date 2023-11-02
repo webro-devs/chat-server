32b9c48bd3be77dd5cad00faceb7f86b5dda094e
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-11-02 12:24:30 +0500

    feat: added changes

---
+++ prisma/migrations/20231102071048_test/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma/migrations/20231102071253_second_test/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Group" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+);
